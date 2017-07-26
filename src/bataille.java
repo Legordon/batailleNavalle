b0a52a48b23126693fe54ef9a6b8b61deabbf51a
Legordon <thegordon@orange.fr>
2017-07-26 22:24:05 +0200

    ajout commentaires

--- src/bataille.java
+++ src/bataille.java
@@ -5,8 +5,12 @@ public class bataille {
 
 	public static void main(String[] args) {
 		int run = 5;
+		
+		// déclaration de 2 tableaux, celui ou est le bateau et celui qui comptabilise les éssais 
 		int[][] mer = new int[10][10];
 		char[][] essai = new char[10][10];
+		
+		// placement du bateau par un seul point sur 2 axes (x, y)
 		int x = (int) (Math.random() * (9+1-0) + 0);
 		int y = (int) (Math.random() * (9+1-0) + 0);
 		System.out.println("Triche debug");
@@ -17,22 +21,25 @@ public class bataille {
 		int testY = 0;
 		Scanner scan = new Scanner(System.in);
 		
+		
+		
+		// tant qu'il reste des éssais le jeu continu
 		while(run != 0) {
 			
 			// check x plus grand que tableau
-			if(x+2 > mer.length){
-				mer[x][y] = 1;
-				mer[x-1][y] = 1;
+			if(x+2 > mer.length){			// si le bateau dépasse du tableau 
+				mer[x][y] = 1;				// on le pose a partir du point
+				mer[x-1][y] = 1;			// et recule jusqu'a x-1 et x-2
 				mer[x-2][y] = 1;
-			} else if (y+2 > mer.length) {
-				mer[x][y] = 1;
+			} else if (y+2 > mer.length) {	// sinon s'il est plus grand sur y 
+				mer[x][y] = 1;				// placement en y-1 y-2
 				mer[x][y-1] = 1;
 				mer[x][y-2] = 1;
 			} else if (x+2 > mer.length && y+2 > mer.length) {
 				mer[x][y] = 1;
 				mer[x-1][y] = 1;
 				mer[x-2][y] = 1;
-			} else {
+			} else {						// sinon placement normal
 				mer[x][y] = 1;
 				mer[x+1][y] = 1;
 				mer[x+2][y] = 1;			
@@ -45,13 +52,14 @@ public class bataille {
 			System.out.println("(0 -> 9");
 			testY = scan.nextInt();
 			
-			if(mer[testX][testY] == 1) {
-				System.out.println("KABOOM !");
+			// test si coordonnées x y correspondent au bateau 
+			if(mer[testX][testY] == 1) {			// si oui 
+				System.out.println("KABOOM !");		// gagné
 				break;
-			} else {
-				System.out.println("plouf...");
-				essai[testX][testY] = 'X';
-				run--;
+			} else {								// sinon 
+				System.out.println("plouf...");		// raté
+				essai[testX][testY] = 'X';			// affichage sur la carte 
+				run--;								// décrémentation essai
 				System.out.println("Il reste "+run+ " essais.");
 				
 			}
